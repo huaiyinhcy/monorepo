b9c5b5f75f5a63879a8d0f74938bc8489d85fc41
何涔涌 <huaiyinhcy@163.com>
2025-09-01 14:50:14 +0800

    删除utils

--- packages/simple-map/src/od/index.ts
+++ packages/simple-map/src/od/index.ts
@@ -5,7 +5,43 @@ import VectorSource from 'ol/source/Vector';
 import type { GetOdLayer } from './types';
 import type { Feature } from 'ol';
 import { borderPreset } from '../presets';
-import { calculateIsoscelesTriangleTopPoint } from '@huaiyinhcy/utils';
+
+/**
+ * 计算等腰三角形顶点坐标
+ * @param point1 - 第一个底角顶点的坐标，格式为 [x, y]
+ * @param point2 - 第二个底角顶点的坐标，格式为 [x, y]
+ * @param angle - 底角角度（单位：度）
+ * @returns 返回一个对象，包含两个可能的顶点坐标
+ */
+const calculateIsoscelesTriangleTopPoint = (
+    [x1, y1]: [number, number],
+    [x2, y2]: [number, number],
+    angle: number
+): {
+    topPoint1: [number, number];
+    topPoint2: [number, number];
+} => {
+    // 将角度转换为弧度
+    const angleRad = angle * (Math.PI / 180);
+    // 计算两点之间的夹角
+    const baseAngle = Math.atan2(y2 - y1, x2 - x1);
+    // 计算两点之间的距离
+    const baseLength = Math.sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1));
+    // 计算等腰三角形的腰长
+    const sideLength = baseLength / 2 / Math.cos(angleRad / 2);
+
+    // 计算两个可能的顶点坐标
+    const topPoint1: [number, number] = [
+        x1 + Math.round(sideLength * Math.cos(baseAngle + angleRad / 2)),
+        y1 + Math.round(sideLength * Math.sin(baseAngle + angleRad / 2)),
+    ];
+    const topPoint2: [number, number] = [
+        x1 + Math.round(sideLength * Math.cos(baseAngle - angleRad / 2)),
+        y1 + Math.round(sideLength * Math.sin(baseAngle - angleRad / 2)),
+    ];
+
+    return { topPoint1, topPoint2 };
+};
 
 export const getOdLayer: GetOdLayer = params => {
     const {
--- packages/utils/src/types.ts
+++
@@ -1,4 +0,0 @@
-export type TreeNode<T> = {
-    [key: string]: any; // 泛型节点类型
-    children?: TreeNode<T>[]; // 子节点数组
-} & T;
